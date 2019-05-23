bfcd29f1ee2adb000c5bd4d554d239897539739f
ngo275 <ngo38114@gmail.com>
2019-05-23 11:11:00 +0700

    init

---
+++ WS-Protobuf-Demo/WS-Protobuf-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WS-Protobuf-Demo
+//
+//  Created by ShuichiNagao on 2019/05/23.
+//  Copyright © 2019 Shuichi Nagao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
